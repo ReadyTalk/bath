02d6e9481ac6dcbc093ddf1f6762fab170bf9daf
Riley Moses <riley.moses@readytalk.com>
2013-08-14 12:47:05 -0600

    Check sudoers in deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,8 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
+
+# TODO:
+# - Check for required packages
+# - Check for www-data user
 
 # Must be root
 who=`whoami`
@@ -17,11 +21,11 @@ then
 fi
 
 # Copy files to correct locations
-SITES_AVAIL="etc/apache2/sites-available"
+SITES_AVAILABLE="etc/apache2/sites-available"
 INIT_D="etc/init.d"
 BATH_DIR="var/lib/bath"
 
-cp -r ${SITES_AVAIL}/port443 /${SITES_AVAIL}/
+cp -r ${SITES_AVAILABLE}/port443 /${SITES_AVAILABLE}/
 cp -r ${INIT_D}/bath /${INIT_D}/
 
 mkdir -p /${BATH_DIR}
@@ -29,6 +33,29 @@ cp -r ${BATH_DIR}/bath.conf /${BATH_DIR}/
 cp -r ${BATH_DIR}/app /${BATH_DIR}/
 
 chown -R www-data:www-data /${BATH_DIR}
+
+# Check sudoers file
+SECURE_PATH_REGEX='Defaults(\s+)secure_path(\s*)=(\s*)"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+CREATESSH_ALIAS_REGEX='Cmnd_Alias(\s+)CREATESSH(\s*)=(\s*)/sbin/iptables -A INPUT -p tcp -m tcp --dport 22 -m state --state NEW -j REJECT -m comment --comment \*'
+ADDSSH_ALIAS_REGEX='Cmnd_Alias(\s+)ADDSSH(\s*)=(\s*)/sbin/iptables -I INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT -s \*'
+DELSSH_ALIAS_REGEX='Cmnd_Alias(\s+)DELSSH(\s*)=(\s*)/sbin/iptables -D INPUT -p tcp --dport 22 -m state --state NEW -j ACCEPT -s \*'
+SHOWSSH_ALIAS_REGEX='Cmnd_Alias(\s+)SHOWSSH(\s*)=(\s*)/sbin/iptables -n -L INPUT'
+
+egrep -q "${SECURE_PATH_REGEX}" /etc/sudoers
+r1=$?
+egrep -q "${CREATESSH_ALIAS_REGEX}" /etc/sudoers
+r2=$?
+egrep -q "${ADDSSH_ALIAS_REGEX}" /etc/sudoers
+r3=$?
+egrep -q "${DELSSH_ALIAS_REGEX}" /etc/sudoers
+r4=$?
+egrep -q "${SHOWSSH_ALIAS_REGEX}" /etc/sudoers
+r5=$?
+if [[ ${r1} -ne 0 ]] || [[ ${r2} -ne 0 ]] || [[ ${r3} -ne 0 ]] || [[ ${r4} -ne 0 ]] || [[ ${r5} -ne 0 ]]
+then
+	echo "The sudoers file needs to be modified. See the README for what must be added."
+	exit 1;
+fi
 
 # Reload/restart apache
 /${INIT_D}/apache2 reload
